cafb016423182f59cd92460eb9491101ce6ffeb0
Nguyễn Duy Thắng <duythang020822@gmail.com>
2024-06-03 15:15:34 +0700

    1st commit

---
+++ routes/index.js
@@ -0,0 +1,11 @@
+var express = require("express");
+var router = express.Router();
+const bookRouter = require("./book.api.js");
+
+/* GET home page. */
+router.get("/", function (req, res, next) {
+  res.status(200).send("Welcome to CoderSchool!");
+});
+router.use("/books", bookRouter);
+
+module.exports = router;
